bc082eeb1d078cc3f620ca3f52170c213f55beaa
Asha Yadav <ayadav@uoregon.edu>
2021-02-08 16:31:48 -0800

    connected plots & tables

--- app.R
+++ app.R
@@ -6,261 +6,112 @@ library(rio)
 library(here)
 library(plotly)
 library(dplyr)
-library(kableExtra)
+library(reactable)
+library(ggthemes)
+
+
 
 data_report <- import(here("data", "data_report.csv"))
 
-view(data_report)
+create_plot <- function(var) {
+  ttl <- case_when(
+    var == "Count_elig" ~ "Eligibility",
+    var == "Count_eval" ~ "Evaluated",
+    var == "Count_ref" ~ "Referrals",
+    var == "Count_qual" ~ "Qualify",
+    var == "Count_exit" ~ "Exit",
+    TRUE ~ "Screen"
+    )
+  
+  data_report %>%
+    ggplot(aes(Year, !!rlang::sym(var))) +
+    geom_line(aes(group = Qualify), color= "grey80") +
+    geom_point(aes(color = Qualify)) + 
+    facet_wrap(~Area, scales = "free_y") + 
+    theme_minimal(base_size = 15) +
+    theme(axis.text.x = element_text(angle = 45,
+                                     vjust = 1.0,
+                                     hjust = 1.0),
+          axis.title.x = element_blank(),
+          axis.title.y = element_blank(),
+          legend.title = element_blank(),
+          plot.title = element_text(hjust = 0.5)) +
+    labs(title = glue::glue("Number of Children in EI & ECSE: {ttl}")) +
+    scale_colour_colorblind()
+}
+
+create_tbl <- function(var) {
+  data_report %>%
+    select(Year, Area, Eligibility, var) %>%
+    pivot_wider(names_from = Eligibility,
+                values_from = all_of(var)) %>%
+    mutate(EI_percent = round((EI/TOTAL)*100, digits = 2)) %>%
+    mutate(ECSE_percent = round((ECSE/TOTAL)*100, digits = 2))
+
+}
+
 
 ui <- dashboardPage(
   dashboardHeader(title = "EC Cares Dashboard"),
   
   
   dashboardSidebar(
-  selectInput("variable", "Select variable: Report Data",
-                choices = c("No. of Children Eligible" = "Eligibile",
-                            "No. of Children Evaluated who Qualify " = "Qualify",
-                            "No. of Children Screened & Not Referred" = "Screen",
-                            "No. of Children Referred" = "Referrals",
-                            "No. of Children who Exit" = "Exit",
-                            "No. of Evaluated who DNQ" = "Evaluated"),
-                selected = "Eligibility")
+    selectInput("var", "Select variable:",
+                choices = c(
+                  "Eligibility" = "Count_elig",
+                  "Evaluated" = "Count_eval",
+                  "Referrals" = "Count_ref",
+                  "Qualify" = "Count_qual",
+                  "Exit" = "Count_exit",
+                  "Screen" = "Count_screen")
+                )
     ),
   dashboardBody(
-    fluidRow(box(plotlyOutput("Eligibility")), box(tableOutput("Eligibility_table"))),
-    fluidRow(box(plotlyOutput("Qualify")), box(tableOutput("Qualify_table"))),
-    fluidRow(box(plotlyOutput("Screen")), box(tableOutput("Screen_table"))),
-    fluidRow(box(plotlyOutput("Referrals")), box(tableOutput("Referrals_table"))),
-    fluidRow(box(plotlyOutput("Exit")), box(tableOutput("Exit_table"))),
-    fluidRow(box(plotlyOutput("Evaluated")), box(tableOutput("Evaluated_table")))
-    )
+    fluidRow(
+      box(plotlyOutput("plot")), 
+      uiOutput("box_tbl")
+    ),
   )
-
-
+)
 
 server <- function(input, output) {
   
-  output$Eligibility <- renderPlotly({
-    
-    d_eligibility<- data_report %>%
-      ggplot(aes(Year, Count_elig)) +
-      geom_line(aes(group = Eligibility), color= "grey80") +
-      geom_point(aes(color = Eligibility)) + 
-      facet_wrap(~Area) + 
-      theme_minimal() +
-      theme(axis.text.x = element_text(angle = 45,
-                                       vjust = 1.0,
-                                       hjust = 1.0),
-            axis.title.x = element_blank(),
-            axis.title.y = element_blank(),
-            legend.title = element_blank(),
-            plot.title = element_text(hjust = 0.5)) +
-      labs(title = "Number of Children Eligible for EI & ECSE") +
-      scale_y_continuous(expand = c(0, 0))
-    ggplotly(d_eligibility)
-
-  
+  output$plot <- renderPlotly({
+    ggplotly(
+      create_plot(input$var)
+    )
   })
   
-  output$Eligibility_table <- function() {
-   
-    data_report %>%
-      select(Year, Area, Eligibility, Count_elig) %>%
-      pivot_wider(names_from = Eligibility,
-                  values_from = Count_elig) %>%
-      mutate(EI_percent = round((EI/TOTAL)*100, digits = 2)) %>%
-      mutate(ECSE_percent = round((ECSE/TOTAL)*100, digits = 2)) %>%
-      kable(caption = "Number of Children Eligible for EI and ECSE") %>%
-      kable_styling(bootstrap_options = "striped", "hover",
-                    full_width = F, position = "float_right") %>%
-      scroll_box(height = "400px", width = "600px")
-      
-      
-  }
+  output$box_tbl <- renderUI({
+    ttl <- case_when(
+      input$var == "Count_elig" ~ "Eligibility",
+      input$var == "Count_eval" ~ "Evaluated",
+      input$var == "Count_ref" ~ "Referrals",
+      input$var == "Count_qual" ~ "Qualify",
+      input$var == "Count_exit" ~ "Exit",
+      TRUE ~ "Screen")
     
-  
-  output$Referrals <- renderPlotly({
-    
-    d_referrals<- data_report %>%
-      ggplot(aes(Year, Count_ref)) +
-      geom_line(aes(group = Referrals), color= "grey80") +
-      geom_point(aes(color = Referrals)) + 
-      facet_wrap(~Area) + 
-      theme_minimal() +
-      theme(axis.text.x = element_text(angle = 45,
-                                       vjust = 1.0,
-                                       hjust = 1.0),
-            axis.title.x = element_blank(),
-            axis.title.y = element_blank(),
-            legend.title = element_blank(),
-            plot.title = element_text(hjust = 0.5)) +
-      labs(title = "Number of Children Referred to EI & ECSE") +
-      scale_y_continuous(expand = c(0, 0))
-    ggplotly(d_referrals)
-  
-  })
-  
-  output$Referrals_table <- function() {
-    
-    data_report %>%
-      select(Year, Area, Referrals, Count_ref) %>%
-      pivot_wider(names_from = Referrals,
-                  values_from = Count_ref) %>%
-      mutate(EI_percent = round((EI/TOTAL)*100, digits = 2)) %>%
-      mutate(ECSE_percent = round((ECSE/TOTAL)*100, digits = 2)) %>%
-      kable(caption = "Number of Children Referred to EI & ECSE") %>%
-      kable_styling(bootstrap_options = "striped", "hover",
-                    full_width = F, position = "float_right") %>%
-      scroll_box(height = "400px", width = "600px") 
-     
-    
-  }
-  
-  
-  output$Qualify <- renderPlotly({
-    
-    d_qualify <- data_report %>%
-      ggplot(aes(Year, Count_qual)) +
-      geom_line(aes(group = Qualify), color= "grey80") +
-      geom_point(aes(color = Qualify)) + 
-      facet_wrap(~Area) + 
-      theme_minimal() +
-      theme(axis.text.x = element_text(angle = 45,
-                                       vjust = 1.0,
-                                       hjust = 1.0),
-            axis.title.x = element_blank(),
-            axis.title.y = element_blank(),
-            legend.title = element_blank(),
-            plot.title = element_text(hjust = 0.5)) +
-      labs(title = "Number of Children Evaluated who Qualify for EI & ECSE") +
-      scale_y_continuous(expand = c(0, 0))
-    ggplotly(d_qualify)
-    
-  })
-  
-  output$Qualify_table <- function() {
-    
-    data_report %>%
-      select(Year, Area, Qualify, Count_qual) %>%
-      pivot_wider(names_from = Qualify,
-                  values_from = Count_qual) %>%
-      mutate(EI_percent = round((EI/TOTAL)*100, digits = 2)) %>%
-      mutate(ECSE_percent = round((ECSE/TOTAL)*100, digits = 2)) %>%
-      kable(caption = "Number of Children Evaluated who Qualify for EI & ECSE") %>%
-      kable_styling(bootstrap_options = "striped", "hover",
-                    full_width = F, position = "float_right") %>%
-      scroll_box(height = "400px", width = "600px") 
-      
-  }
- 
-  
-  output$Exit <- renderPlotly({
-    
-    d_exit <- data_report %>%
-      ggplot(aes(Year, Count_exit)) +
-      geom_line(aes(group = Exit), color= "grey80") +
-      geom_point(aes(color = Exit)) + 
-      facet_wrap(~Area) + 
-      theme_minimal() +
-      theme(axis.text.x = element_text(angle = 45,
-                                       vjust = 1.0,
-                                       hjust = 1.0),
-            axis.title.x = element_blank(),
-            axis.title.y = element_blank(),
-            legend.title = element_blank(),
-            plot.title = element_text(hjust = 0.5)) +
-      labs(title = "Number of Children who Exit from EI & ECSE") +
-      scale_y_continuous(expand = c(0, 0))
-    ggplotly(d_exit)
-    
-  })
-  
-  output$Exit_table <- function() {
-    
-    data_report %>%
-      select(Year, Area, Exit, Count_exit) %>%
-      pivot_wider(names_from = Exit,
-                  values_from = Count_exit) %>%
-      mutate(EI_percent = round((EI/TOTAL)*100, digits = 2)) %>%
-      mutate(ECSE_percent = round((ECSE/TOTAL)*100, digits = 2)) %>%
-      kable(caption = "Number of Children who Exit from EI & ECSE") %>%
-      kable_styling(bootstrap_options = "striped", "hover",
-                    full_width = F, position = "float_right") %>%
-      scroll_box(height = "400px", width = "600px") 
-     
-  } 
-  
-  
-  output$Screen <- renderPlotly({
-    
-    d_screen <- data_report %>%
-      ggplot(aes(Year, Count_screen)) +
-      geom_line(aes(group = Screen), color= "grey80") +
-      geom_point(aes(color = Screen)) + 
-      facet_wrap(~Area) + 
-      theme_minimal() +
-      theme(axis.text.x = element_text(angle = 45,
-                                       vjust = 1.0,
-                                       hjust = 1.0),
-            axis.title.x = element_blank(),
-            axis.title.y = element_blank(),
-            legend.title = element_blank(),
-            plot.title = element_text(hjust = 0.5)) +
-      labs(title = "Number of Children Screened and Not Referred for Evaluation") +
-      scale_y_continuous(expand = c(0, 0))
-    ggplotly(d_screen)
-    
-  })
-  
-  output$Screen_table <- function() {
-    data_report %>%
-      select(Year, Area, Screen, Count_screen) %>%
-      pivot_wider(names_from = Screen,
-                  values_from = Count_screen) %>%
-      mutate(EI_percent = round((EI/TOTAL)*100, digits = 2 )) %>%
-      mutate(ECSE_percent = round((ECSE/TOTAL)*100, digits = 2)) %>%
-      kable(caption = "Number of Children Screened and Not Referred for Evaluation") %>%
-      kable_styling(bootstrap_options = "striped", "hover",
-                    full_width = F, position = "float_right") %>%
-      scroll_box(height = "400px", width = "600px") 
-     
-  }
-  
-  
-  output$Evaluated <- renderPlotly({
-    
-    d_eval <- data_report %>%
-      ggplot(aes(Year, Count_eval)) +
-      geom_line(aes(group = Evaluated), color= "grey80") +
-      geom_point(aes(color = Evaluated)) + 
-      facet_wrap(~Area) + 
-      theme_minimal() +
-      theme(axis.text.x = element_text(angle = 45,
-                                       vjust = 1.0,
-                                       hjust = 1.0),
-            axis.title.x = element_blank(),
-            axis.title.y = element_blank(),
-            legend.title = element_blank(),
-            plot.title = element_text(hjust = 0.5)) +
-      labs(title = "Number of Children Evaluated who Did Not Qualify") +
-      scale_y_continuous(expand = c(0, 0))
-    ggplotly(d_eval)
-    
+    box(title = ttl,
+        reactable(
+          create_tbl(input$var), highlight = TRUE, 
+          theme = reactableTheme(
+            highlightColor = "#56B4E9",
+            headerStyle = list(
+              "&:hover[aria-sort]" = list(background = "hsl(0, 0%, 96%)"),
+              "&[aria-sort='ascending'], 
+              &[aria-sort='descending']" = list(background = "hsl(0, 0%, 96%)"),
+              borderColor = "#555")
+            
+            )
+          ))
     })
   
-  output$Evaluated_table <- function() {
-    data_report %>%
-      select(Year, Area, Evaluated, Count_eval) %>%
-      pivot_wider(names_from = Evaluated,
-                  values_from = Count_eval) %>%
-      mutate(EI_percent = round((EI/TOTAL)*100, digits = 2)) %>%
-      mutate(ECSE_percent = round((ECSE/TOTAL)*100, digits = 2)) %>%
-      kable(caption = "Number of Children Evaluated who Did Not Qualify") %>%
-      kable_styling(bootstrap_options = "striped", "hover",
-                    full_width = F, position = "float_right") %>%
-      scroll_box(height = "400px", width = "600px")
-    }
-  
-  }
+  output$tbl <- renderReactable({
+   reactable(
+     create_tbl(input$var)
+   )
+  })
+    
+}
 
 shinyApp(ui, server)
